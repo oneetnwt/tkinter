4da19dc2178f66c28049d08c4b9ef68fc24da2a4
Jeff Ivan <jeffivanbiosanomayor@gmail.com>
2025-01-01 22:54:46 +0800

    Add new files

---
+++ main.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class MyGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.label = tk.Label(self.root, text="Your Message", font=('Arial', 18))
+        self.label.pack(padx=10, pady=10)
+
+        self.textbox = tk.Text(self.root, height=5, font=('Arial', 16))
+        self.textbox.bind("<KeyPress>", self.shortcut)
+        self.textbox.pack(padx=10, pady=10)
+
+        self.check_state = tk.IntVar()
+
+        self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=('Arial', 16), variable=self.check_state)
+        self.check.pack(padx=10, pady=10)
+
+        self.button = tk.Button(self.root, text="Show Message", font=('Arial', 18), command=self.show_message)
+        self.button.pack(padx=10, pady=10)
+
+        self.root.mainloop()
+
+    def show_message(self):
+        if self.check_state.get() == 0:
+            print(self.textbox.get('1.0', tk.END))
+        else:
+            messagebox.showinfo(title="Message", message=self.textbox.get('1.0', tk.END))
+
+    def shortcut(self, event):
+        if event.state == 12 and event.keysym == "Return":
+            self.show_message()
+
+def main():
+    gui = MyGUI()
+
+if __name__ == "__main__":
+    main()
